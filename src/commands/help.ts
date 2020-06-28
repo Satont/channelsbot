69602e43cf8a05d2f907e2a308ee2e97736127de
Andrey <CLWJerri@yandex.ua>
2020-06-29 01:22:56 +0300

    Change sum to commandName

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -5,11 +5,11 @@ export default {
   name: 'help',
   aliases: ['помощь'],
   async run(msg) {
-    const sum = msg.content.split(' ')[0]
+    const commandName = msg.content.split(' ')[0]
     const embed = new MessageEmbed({
       thumbnail: { url: msg.client.user.avatarURL() },
       fields: [
-        { name: sum, value: msg.guild.lang.get('commands.help.fields.helpCommand'), inline: false },
+        { name: commandName, value: msg.guild.lang.get('commands.help.fields.helpCommand'), inline: false },
         { name: 'cc!lang russian/english', value: msg.guild.lang.get('commands.help.fields.langSelector'), inline: false },
         { name: 'cc!kick @user', value: msg.guild.lang.get('commands.help.fields.kickUser'), inline: false },
         { name: 'cc!list', value: msg.guild.lang.get('commands.help.fields.listChannels'), inline: false },
