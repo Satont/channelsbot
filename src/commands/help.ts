737e593441ddbcff2bea585b2d2a153bca4de495
LWJerri <50290430+LWJerri@users.noreply.github.com>
2020-06-30 16:53:14 +0300

    Add command cc!info to menu list

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -10,6 +10,7 @@ export default {
       thumbnail: { url: msg.client.user.avatarURL() },
       fields: [
         { name: commandName, value: msg.guild.lang.get('commands.help.fields.helpCommand'), inline: false },
+        { name: 'cc!info', value: msg.guild.lang.get('commands.help.fields.infoCommand'), inline: false },
         { name: 'cc!lang russian/english', value: msg.guild.lang.get('commands.help.fields.langSelector'), inline: false },
         { name: 'cc!kick @user', value: msg.guild.lang.get('commands.help.fields.kickUser'), inline: false },
         { name: 'cc!list', value: msg.guild.lang.get('commands.help.fields.listChannels'), inline: false },
