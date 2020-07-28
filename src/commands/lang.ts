fa4c73292935d420a84788dda06644486efc728f
LWJerri <50290430+LWJerri@users.noreply.github.com>
2020-07-28 19:14:06 +0300

    Removed space

--- src/commands/lang.ts
+++ src/commands/lang.ts
@@ -8,7 +8,6 @@ export default {
   checkCustomPerm(msg) {
     return (msg.member.hasPermission('ADMINISTRATOR') || msg.member.hasPermission('MANAGE_GUILD'))
   },
-
   async run(msg, args) {
     const lang = args[0]
     if (lang !== 'russian' && lang !== 'english') return msg.reply(msg.guild.lang.get('commands.lang.notExist', lang))
